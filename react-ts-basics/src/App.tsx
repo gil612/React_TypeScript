1ac04e33c92d2deb2f9eec22b488f5968bc9346e
gil612 <sgba000@gmail.com>
2024-03-31 23:05:30 +0200

    output state based values

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -1,7 +1,8 @@
+import { useState } from 'react';
+
 import CourseGoal from './components/CourseGoal.tsx';
 import Header from './components/Header.tsx';
 import goalsImg from './assets/goals.jpg';
-import { useState } from 'react';
 
 type CourseGoal = {
   title: string;
@@ -10,56 +11,36 @@ type CourseGoal = {
 }
 
 export default function App() {
-  const [goals, setGoals] = useState<Array<CourseGoal[]>([]);
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
 
   function handleAddGoal() {
-    setGoals(() => {});
-
+    // () => {}
+    setGoals(prevGoals => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: 'Learn React + TS',
+        description: 'Learn it in depth!'
+      };
+      // returned new state snapshot
+      return [...prevGoals, newGoal]
+    });
   }
+
   return (
     <main>
       <Header image = {{src: goalsImg, alt: "A list of goals"}} >
         <h1>Your Course Goals</h1>
         </Header>
         <button onClick={handleAddGoal}>Add Goal</button>
-        <CourseGoal title="learn React + TS">
-        <p>Learn it from the ground up</p>
-      </CourseGoal>
+        <ul>
+          {goals.map((goal) => (
+            <li key={goal.id}>
+              <CourseGoal title={goal.title}>
+                <p>{goal.description}</p>
+              </CourseGoal>
+            </li>
+          ))}
+        </ul>
     </main>
   );
 }
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
