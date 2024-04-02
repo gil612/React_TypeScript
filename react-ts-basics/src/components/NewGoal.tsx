7f2e7a61672f0b1764845f10afbe94a68e90e126
gil612 <sgba000@gmail.com>
2024-04-02 14:53:15 +0200

    Working with Event Types

--- react-ts-basics/src/components/NewGoal.tsx
+++ react-ts-basics/src/components/NewGoal.tsx
@@ -1,19 +1,21 @@
 import { FormEvent } from "react";
 
 export default function NewGoal() {
-    function handleSubmit(event: FormEvent) {
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
+
+        new FormData(event.currentTarget);
     }
     
     return  (
     <form onSubmit={handleSubmit}>
         <p>
             <label htmlFor="goal">Your Goal</label>
-            <input id="goal" type="text"></input>
+            <input id="goal" type="text"/>
         </p>
         <p>
             <label htmlFor="summary">Short Summary</label>
-            <input id="summary" type="text"></input>
+            <input id="summary" type="text" />
         </p>
         <p>
             <button>Add Goal</button>
