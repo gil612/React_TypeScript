a9f46332269bccc74729fa91b3c52438a43cdb24
gil612 <sgba000@gmail.com>
2024-04-06 16:18:44 +0200

    Building components with discriminated unions

--- react-ts-basics/src/components/CourseGoalList.tsx
+++ react-ts-basics/src/components/CourseGoalList.tsx
@@ -25,7 +25,7 @@ export default function CourseGoalList({
   
        if (goals.length >= 2) {
     warningBox = (
-      <InfoBox mode = "warning" severity='high'>
+      <InfoBox mode = "warning" severity='medium'>
       You're collecting a lot of goals. Don't put too much on your plate!
     </InfoBox>
     );
@@ -46,50 +46,3 @@ export default function CourseGoalList({
       </>
       );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   if (goals.length === 0) {
-//     return (
-//     <InfoBox mode = "hint">
-//       You have no course goals yet. Start adding some!
-//       </InfoBox>
-//     );
-//   }
-//   let warningBox: ReactNode;
-
-//   if (goals.length >= 4) {
-//     warningBox = (
-//       <InfoBox mode = "warning">
-//       You're collecting a lot of goals. Don't put too much on your plate!
-//     </InfoBox>
-//     );
-//   }
-
-//     return (
-//       <>
-//       {warningBox}
-//         <ul>
-//           {goals.map((goal) => (
-//             <li key={goal.id}>
-//               <CourseGoal id={goal.id} title={goal.title} onDelete= {onDeleteGoal}>
-//                 <p>{goal.description}</p>
-//                 <p> ID of this goal: {goal.id}</p>
-//               </CourseGoal>
-//             </li>
-//           ))}
-//         </ul>
-//         );
-// }
