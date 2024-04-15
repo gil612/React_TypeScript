91e21d8557f2fb45b3276734414586f22287e605
gil612 <sgba000@gmail.com>
2024-04-15 12:17:47 +0200

    Custom Form Component

--- react-ts-basics_2/src/App.tsx
+++ react-ts-basics_2/src/App.tsx
@@ -1,13 +1,22 @@
 import { useRef } from 'react';
 
 import Input from './components/Input.tsx';
+import Button from './components/Button.tsx';
+import Form from './components/Form.tsx';
 
 function App() {
   const input = useRef<HTMLInputElement>(null);
   
   return (
   <main>
-    <Input label="Test" id = "test"  ref = {input} />
+    <Form>
+    <Input type="text" label="Name" id = "name" />
+    <Input type="number" label="Age" id = "age" />
+    <p>
+      <Button>Save</Button>
+    </p>
+    </Form>
+    
   </main>
   );
 }
